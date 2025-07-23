79879fea2f5191b892c9cc860c5ef60f2dc4f2f5
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2025-07-23 13:15:34 -0700

    Add map webpage and static files

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(import.meta.dirname, 'public', 'css')));
+app.use(express.static(path.join(import.meta.dirname, 'public', 'js')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(import.meta.dirname, 'index.html'));
+})
+
+app.listen(port, () => {})
+console.log(`Server running at http://localhost:${port}`);
